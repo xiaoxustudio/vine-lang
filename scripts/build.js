3effec8fb1066c31f5e6211f78113db4b1dedd48
xiaoxustudio <1783558957@qq.com>
2025-06-05 15:42:25 +0800

    update: 修改build 脚本

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,6 @@
 const rcedit = require("rcedit");
 const { exec } = require("pkg");
-exec([
-	"output/index.js",
-	"--target",
-	"node18",
-	"--platform",
-	"windows",
-	"--output",
-	"output/vine",
-]).then(() => {
+exec(["output/index.js", "--output", "output/vine"]).then(() => {
 	rcedit("output/vine.exe", {
 		icon: "resources/icon.ico",
 	});
