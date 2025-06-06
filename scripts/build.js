d206b9848e5d9e1737544bbdd04acf680887493e
xiaoxustudio <1783558957@qq.com>
2025-06-06 23:32:15 +0800

    fix: 暂时注释修改icon代码

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,16 @@
-const rcedit = require("rcedit");
+// const rcedit = require("rcedit");
 const { exec } = require("pkg");
-exec(["output/index.js", "--output", "output/vine"]).then(() => {
-	rcedit("output/vine.exe", {
-		icon: "resources/icon.ico",
-	});
-});
+
+async function build() {
+	await exec([
+		"output/index.js",
+		"--target",
+		"node18",
+		"--output",
+		"output/vine",
+	]);
+	// await rcedit("output/vine.exe", {
+	// 	icon: "resources/icon.ico",
+	// });
+}
+build();
