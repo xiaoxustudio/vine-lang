61928b9322caa4f91d7419ba0b299f91ca895328
xiaoxustudio <1783558957@qq.com>
2025-06-30 11:58:45 +0800

    chore: 移除rcedit

--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,6 @@
-// const rcedit = require("rcedit");
 const { exec } = require("@yao-pkg/pkg");
-const { copyFileSync } = require("fs");
 
 async function build() {
 	await exec(["dist/index.js", "--target", "--output", "dist/vine"]);
-	copyFileSync("README.md", "dist/README.md");
-	copyFileSync("README_CN.md", "dist/README_CN.md");
-	copyFileSync("package.json", "dist/package.md");
-	copyFileSync("LICENSE", "dist/LICENSE");
-	// await rcedit("output/vine.exe", {
-	// 	icon: "resources/icon.ico",
-	// });
 }
 build();
