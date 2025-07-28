9f80e193ff9650364121054fb0358cddf75f501d
xiaoxustudio <1783558957@qq.com>
2025-07-29 00:11:04 +0800

    chore: change rollup to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+	entry: ["src/**/*.ts"],
+	outDir: "dist",
+	format: isProduction ? ["cjs", "esm"] : ["cjs"],
+	splitting: false,
+	treeshake: false,
+	dts: isProduction,
+	target: "es2020",
+	clean: true,
+	bundle: false,
+	define: {
+		"process.env.NODE_ENV": JSON.stringify(
+			isProduction ? "production" : "development"
+		),
+	},
+});
